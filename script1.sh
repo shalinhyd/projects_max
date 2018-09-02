6a5a9556dc5aac116758822372af2c6cdc8cc118
shalinhyd <42828580+shalinhyd@users.noreply.github.com>
2018-09-02 21:09:45 +0530

    Delete script1.sh

--- script1.sh
+++
@@ -1,9 +0,0 @@
-if [ -f  /tmp/line.txt ]; then
-  echo  "This is line one";
-  echo  "This is line two";
-  echo  "This is line three";
-  echo  "This is line four" ;
-else 
-  echo "error in the lines " 
-fi
-
